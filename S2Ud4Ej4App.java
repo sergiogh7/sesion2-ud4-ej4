da629f11dae2537e80a134fec3b57a59dcb03b33
Sergio <87013586+sergiogh7@users.noreply.github.com>
2021-07-07 10:11:49 +0200

    Sesion2-Ud4-Ej4

---
+++ S2Ud4Ej4App.java
@@ -0,0 +1,20 @@
+/**
+ * Version 1.0
+ * @author Sergio Galimany Hoyo
+ * 
+ */
+public class S2Ud4Ej4App {
+
+	public static void main(String[] args) {
+		
+		//Variables.
+		int N = 1;
+		
+		// Salida.
+		System.out.println("Valor inicial de N: " + N);
+		System.out.println("Incrementa en 77: " + (N += 77) );
+		System.out.println( "Decrementa en 3: " + (N -= 3) );
+		System.out.println("Duplica su valor: " + (N = N*2) );
+
+	}
+}
